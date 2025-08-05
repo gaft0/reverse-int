8be878758d81f6f99e2155c00a7f46a23bb1b418
Александр Плесовских <dmitx04@gmail.com>
2025-08-05 21:13:21 +0300

    fix: fix 2 errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 module.exports = function reverse(n) {
-  const array = n.toString().split('').reverse();
+  let count;
+  let positiveN;
+  if (n < 0) {
+    count = 1;
+    positiveN = Math.abs(n);
+  } else {
+    count = 0;
+    positiveN = n;
+  }
+  const array = positiveN.toString().split('').reverse();
   const string = array.toString();
   let newString = '';
-  for (let i = 0; i < string.length; i++) {
+  for (let i = 0; i < string.length; i += 1) {
     if (string[i] !== ',') {
       newString += string[i];
     }
   }
   const number = Number(newString);
+  if (count === 1) {
+    return number;
+  }
   return number;
-}
+};
