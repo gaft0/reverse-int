63427fab861a75f959f7c45b10f5054139ab3dfc
Александр Плесовских <dmitx04@gmail.com>
2025-08-05 20:18:14 +0300

    feat: make an algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
-};
+module.exports = function reverse(n) {
+  const array = n.toString().split('').reverse();
+  const string = array.toString();
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== ',') {
+      newString += string[i];
+    }
+  }
+  const number = Number(newString);
+  return number;
+}
